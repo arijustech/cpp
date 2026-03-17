57a1c0e3f38746cd05e48bb2966a7f85d1a25442
arijustech <arijus.ka@outlook.com>
2026-03-17 17:31:22 +0200

    update

--- programming/programming/L1_2.cpp
+++ programming/programming/L1_2.cpp
@@ -2,7 +2,36 @@
 using namespace std;
 
 int main() {
-	int x;
-	int y;
+	double x;
+	double y;
 
+	cout << "Yveskite x reikshme: ";
+	cin >> x;
+	cout << "Yveskite y reikshme: ";
+	cin >> y;
+
+	if (x == 0 and y == 0) {
+		cout << x << " ir " << y << " yra pradzios taske";
+	}
+	else if (x == 0) {
+		cout << "y asis";
+	}
+	else if (y == 0) {
+		cout << "x asis";
+	}
+
+	else if (x > 0 and y > 0) {
+		cout << x << " ir " << y << "yra pirmajame ketvirtyje";
+	}
+	else if (x < 0 and y > 0) {
+		cout << x << " ir " << y << "yra antrame ketvirtyje";
+	}
+	else if (x < 0 and y < 0) {
+		cout << x << " ir " << y << "yra treciame ketvirtyje";
+	}
+	else if (x > 0 and y < 0) {
+		cout << x << " ir " << y << "yra ketvirtame ketvirtyje";
+	}
+
+	return 0;
 }
